ad475a17d22677c926abf869d80992a0f6a5f3d1
Harry Smith <harry@hsmith.net>
2023-10-26 17:34:05 +0800

    Basic git cat-file command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-#[allow(unused_imports)]
-use std::env;
-#[allow(unused_imports)]
+use clap::Args;
+use flate2::read::ZlibDecoder;
 use std::fs;
+use std::io::prelude::*;
 
 use clap::{Parser, Subcommand};
 
@@ -14,6 +14,14 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Init,
+    CatFile(CatFileArgs),
+}
+
+#[derive(Debug, Args)]
+struct CatFileArgs {
+    #[arg(short)]
+    pretty_print: bool,
+    object: String,
 }
 
 fn git_init() {
@@ -24,10 +32,37 @@ fn git_init() {
     println!("Initialized git directory")
 }
 
+fn git_cat_file(args: &CatFileArgs) {
+    let directory = args.object.chars().take(2).collect::<String>();
+    let filename = args.object.chars().skip(2).collect::<String>();
+    let path = format!(".git/objects/{}/{}", directory, filename);
+
+    eprintln!("Object: {}", args.object);
+    eprintln!("Opening .git/objects/{}/{}", directory, filename);
+
+    let file = fs::read(path).unwrap();
+
+    eprintln!("File: {:?}", file);
+
+    let mut decompressed = ZlibDecoder::new(&*file);
+    let mut contents = String::new();
+    decompressed.read_to_string(&mut contents).unwrap();
+
+    let contents = contents.split('\0').collect::<Vec<&str>>();
+    let (_tipe, length) = contents[0].split_once(' ').unwrap();
+    let length: usize = length.parse().unwrap();
+
+    let blob_contents = contents[1][0..length].to_string();
+    print!("{}", blob_contents);
+}
+
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
         Commands::Init => git_init(),
+        Commands::CatFile(args) => {
+            git_cat_file(args);
+        }
     }
 }

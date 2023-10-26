9f7353b08d7bcb21e6ac9da7f503029f0809dd41
Harry Smith <harry@hsmith.net>
2023-10-26 17:39:59 +0800

    Introduce GitObject enum

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,10 @@ struct CatFileArgs {
     object: String,
 }
 
+enum GitObject {
+    Blob { length: usize, contents: String },
+}
+
 fn git_init() {
     fs::create_dir(".git").unwrap();
     fs::create_dir(".git/objects").unwrap();
@@ -37,23 +41,32 @@ fn git_cat_file(args: &CatFileArgs) {
     let filename = args.object.chars().skip(2).collect::<String>();
     let path = format!(".git/objects/{}/{}", directory, filename);
 
-    eprintln!("Object: {}", args.object);
-    eprintln!("Opening .git/objects/{}/{}", directory, filename);
-
     let file = fs::read(path).unwrap();
-
-    eprintln!("File: {:?}", file);
 
     let mut decompressed = ZlibDecoder::new(&*file);
     let mut contents = String::new();
     decompressed.read_to_string(&mut contents).unwrap();
 
     let contents = contents.split('\0').collect::<Vec<&str>>();
-    let (_tipe, length) = contents[0].split_once(' ').unwrap();
+    let (tipe, length) = contents[0].split_once(' ').unwrap();
     let length: usize = length.parse().unwrap();
 
-    let blob_contents = contents[1][0..length].to_string();
-    print!("{}", blob_contents);
+    let object = match tipe {
+        "blob" => GitObject::Blob {
+            length,
+            contents: contents[1][0..length].to_string(),
+        },
+        _ => panic!("Unknown object type: {}", tipe),
+    };
+
+    match object {
+        GitObject::Blob {
+            length: _,
+            contents,
+        } => {
+            print!("{}", contents);
+        }
+    }
 }
 
 fn main() {

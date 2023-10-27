657f911f45ca11ad59878069c56d5faddd837a0f
Harry Smith <harry@hsmith.net>
2023-10-27 17:13:11 +0800

    Add git ls-tree command

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ enum Commands {
     Init,
     CatFile(CatFileArgs),
     HashObject(HashObjectArgs),
+    LsTree(LsTreeArgs),
 }
 
 #[derive(Debug, Args)]
@@ -31,6 +32,13 @@ struct HashObjectArgs {
     #[arg(short)]
     write: bool,
     path: PathBuf,
+}
+
+#[derive(Debug, Args)]
+struct LsTreeArgs {
+    #[arg(long)]
+    name_only: bool,
+    object: String,
 }
 
 trait HasObjectHash {
@@ -64,14 +72,72 @@ impl<T: HasObjectHash> GitPath for T {
         format!(".git/objects/{}", directory).into()
     }
 }
+
 enum GitObject {
     Blob(Blob),
+    Tree(Tree),
 }
 
 struct Blob {
     object_hash: String,
     length: usize,
     contents: String,
+}
+
+#[derive(Debug)]
+struct Tree {
+    object_hash: String,
+    entries: Vec<TreeEntry>,
+}
+
+impl Tree {
+    fn new(object_hash: String, entries: Vec<TreeEntry>) -> Self {
+        Self {
+            object_hash,
+            entries,
+        }
+    }
+
+    fn from_tree_file(contents: &str) -> Self {
+        let (header, rest) = contents.split_once('\0').unwrap();
+        let length: usize = header.split(' ').nth(1).unwrap().parse().unwrap();
+        let entries = rest
+            .split('\n')
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                let (mode, rest) = line.split_once(' ').unwrap();
+                let (name, object_hash) = rest.split_once('\0').unwrap();
+                TreeEntry {
+                    mode: mode.to_string(),
+                    object_hash: object_hash.to_string(),
+                    name: name.to_string(),
+                }
+            })
+            .collect::<Vec<TreeEntry>>();
+
+        let mut hasher = Sha1::new();
+        hasher.update(contents.as_bytes());
+        let result = hasher.finalize();
+        let object_hash = format!("{:x}", result);
+
+        Self {
+            object_hash,
+            entries,
+        }
+    }
+}
+
+impl HasObjectHash for Tree {
+    fn hash(&self) -> &str {
+        &self.object_hash
+    }
+}
+
+#[derive(Debug)]
+struct TreeEntry {
+    mode: String,
+    object_hash: String,
+    name: String,
 }
 
 impl Blob {
@@ -100,6 +166,7 @@ impl Blob {
         }
     }
 
+    // TODO: Writing git objects can move to trait and generic function can handle writing the output.
     fn header(&self) -> String {
         format!("blob {}\0", self.length)
     }
@@ -107,7 +174,7 @@ impl Blob {
     fn as_bytes(&self) -> Vec<u8> {
         format!("{}{}", self.header(), self.contents).into_bytes()
     }
-    }
+}
 
 impl HasObjectHash for Blob {
     fn hash(&self) -> &str {
@@ -123,32 +190,42 @@ fn git_init() {
     println!("Initialized git directory")
 }
 
-fn git_cat_file(args: &CatFileArgs) {
-    let path = Blob::path_from_object_hash(&args.object);
-
+fn load_git_object_from_hash(object: &str) -> GitObject {
+    let path = Blob::path_from_object_hash(object);
     let file = fs::read(path).unwrap();
 
-    let mut decompressed = ZlibDecoder::new(&*file);
-    let mut contents = String::new();
-    decompressed.read_to_string(&mut contents).unwrap();
+    let mut decompressed = ZlibDecoder::new(&file[..]);
+    let mut buf = Vec::new();
+    decompressed.read_to_end(&mut buf).unwrap();
+    let contents = String::from_utf8_lossy(&buf);
 
-    let contents = contents.split('\0').collect::<Vec<&str>>();
-    let (tipe, length) = contents[0].split_once(' ').unwrap();
+    let split_contents = contents.split('\0').collect::<Vec<&str>>();
+    let (tipe, length) = split_contents[0].split_once(' ').unwrap();
     let length: usize = length.parse().unwrap();
 
-    let object = match tipe {
-        "blob" => GitObject::Blob(Blob::new(
-            args.object.clone(),
-            length,
-            contents[1][0..length].to_string(),
-        )),
+    // TODO: We should homogonise a deserialise/serialize function for each object struct, maybe as trait
+    match tipe {
+        "blob" => {
+            let blob = Blob::new(
+                object.to_string(),
+                length,
+                split_contents[1][0..length].to_string(),
+            );
+            GitObject::Blob(blob)
+        }
+        "tree" => GitObject::Tree(Tree::from_tree_file(&contents)),
         _ => panic!("Unknown object type: {}", tipe),
-    };
+    }
+}
+
+fn git_cat_file(args: &CatFileArgs) {
+    let object = load_git_object_from_hash(&args.object);
 
     match object {
         GitObject::Blob(blob) => {
             print!("{}", blob.contents);
         }
+        GitObject::Tree(_) => todo!("git cat-file <tree> needs implementing!"),
     }
 }
 
@@ -169,9 +246,23 @@ fn git_hash_object(args: &HashObjectArgs) {
     println!("{}", blob.object_hash);
 }
 
+fn git_ls_tree(args: &LsTreeArgs) {
+    let object = load_git_object_from_hash(&args.object);
+
+    match object {
+        GitObject::Blob(_) => panic!("git ls-tree <blob> not implemented!"),
+        GitObject::Tree(tree) => {
+            for entry in tree.entries {
+                println!("{}", entry.name);
+            }
+        }
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
+    // TODO: Macro this
     match &cli.command {
         Commands::Init => git_init(),
         Commands::CatFile(args) => {
@@ -179,6 +270,9 @@ fn main() {
         }
         Commands::HashObject(args) => {
             git_hash_object(args);
+        }
+        Commands::LsTree(args) => {
+            git_ls_tree(args);
         }
     }
 }

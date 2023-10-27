2847eaa1aa5bc70e43e4a0981574949c5d32af87
Harry Smith <harry@hsmith.net>
2023-10-27 16:08:36 +0800

    Add 'git hash-object'

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Args;
-use flate2::read::ZlibDecoder;
-use std::fs;
+use flate2::{read::ZlibDecoder, write::ZlibEncoder};
+use sha1::{Digest, Sha1};
 use std::io::prelude::*;
+use std::{fs, path::PathBuf};
 
 use clap::{Parser, Subcommand};
 
@@ -15,6 +16,7 @@ struct Cli {
 enum Commands {
     Init,
     CatFile(CatFileArgs),
+    HashObject(HashObjectArgs),
 }
 
 #[derive(Debug, Args)]
@@ -22,6 +24,13 @@ struct CatFileArgs {
     #[arg(short)]
     pretty_print: bool,
     object: String,
+}
+
+#[derive(Debug, Args)]
+struct HashObjectArgs {
+    #[arg(short)]
+    write: bool,
+    path: PathBuf,
 }
 
 enum GitObject {
@@ -69,6 +78,33 @@ fn git_cat_file(args: &CatFileArgs) {
     }
 }
 
+fn git_hash_object(args: &HashObjectArgs) {
+    let contents = fs::read_to_string(&args.path).unwrap();
+    let header = format!("blob {}\0", contents.len());
+    let store = format!("{}{}", header, contents);
+
+    let mut hasher = Sha1::new();
+    hasher.update(store.as_bytes());
+    let result = hasher.finalize();
+    let hash = format!("{:x}", result);
+
+    let directory = hash.chars().take(2).collect::<String>();
+    let filename = hash.chars().skip(2).collect::<String>();
+    let path = format!(".git/objects/{}/{}", directory, filename);
+
+    if args.write {
+        fs::create_dir_all(format!(".git/objects/{}", directory)).unwrap();
+
+        let mut encoder = ZlibEncoder::new(Vec::new(), flate2::Compression::default());
+        encoder.write_all(store.as_bytes()).unwrap();
+
+        let compressed = encoder.finish().unwrap();
+        fs::write(path, compressed).unwrap();
+    }
+
+    println!("{}", hash);
+}
+
 fn main() {
     let cli = Cli::parse();
 
@@ -76,6 +112,9 @@ fn main() {
         Commands::Init => git_init(),
         Commands::CatFile(args) => {
             git_cat_file(args);
+        }
+        Commands::HashObject(args) => {
+            git_hash_object(args);
         }
     }
 }
